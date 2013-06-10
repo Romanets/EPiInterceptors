98ffad938ee6759580b4c136335090001edef164
Roman Mironets <roman.mironets@creuna.no>
2013-06-10 11:43:23 +0300

    Created nuget package spec, target framewrk switched to 4.0

--- EPiInterceptors/Properties/AssemblyInfo.cs
+++ EPiInterceptors/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EPiInterceptors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides an easy way to extend EPiServer data content interception pipeline.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Creuna")]
 [assembly: AssemblyProduct("EPiInterceptors")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
